3380b01bbbff30c9a8d3b8ac2a1647d44a438c77
bcongelio <congelio@kutztown.edu>
2023-03-19 00:15:23 -0400

    major data viz update

--- book-functions.R
+++ book-functions.R
@@ -18,3 +18,27 @@ style_data <- function(dat, n_rows = NULL, caption = NULL) {
 
 # Set the knitr options
 knitr::opts_chunk$set(warning = FALSE, message = FALSE, out.width = "100%")
+
+
+### creating theme used in ggplot
+nfl_analytics_theme <- function(..., base_size = 12) {
+  
+  theme(
+    text = element_text(family = "Roboto Condensed", size = base_size, color = "black"),
+    axis.ticks = element_blank(),
+    axis.title = element_text(face = "bold"),
+    axis.text = element_text(face = "bold"),
+    plot.title.position = "plot",
+    plot.title = element_text(size = 16,
+                              face = "bold",
+                              vjust = .02,
+                              hjust = 0.5),
+    plot.subtitle = element_text(hjust = 0.5),
+    plot.caption = element_text(size = 8,
+                                face = "italic"),
+    panel.grid.minor = element_blank(),
+    panel.grid.major =  element_line(color = "#d0d0d0"),
+    panel.background = element_rect(fill = "#f7f7f7"),
+    plot.background = element_rect(fill = "#f7f7f7"),
+    panel.border = element_blank())
+}
