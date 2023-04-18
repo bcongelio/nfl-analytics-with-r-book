ca2441989d297312e4fd5b40cde7eaefa661b1d2
bcongelio <congelio@kutztown.edu>
2023-04-17 21:55:43 -0400

    model finished

--- example_data/R/playtype_multinomial_coding.R
+++ example_data/R/playtype_multinomial_coding.R
@@ -6,6 +6,9 @@ library(data.table)
 library(nnet)
 library(caret)
 library(doParallel)
+library(gt)
+library(gtExtras)  
+
 
 
 pbp <- nflreadr::load_pbp(2006:2022) %>%
@@ -87,55 +90,86 @@ model_data_clean <- model_data_clean %>%
 
 model_data_clean <- na.omit(model_data_clean)
 
+
+###########
+## splitting and running
+###########
+
 set.seed(1984)
-play_pred_split <- rsample::initial_split(model_data_clean, 0.80, strata = play_call)
+
+play_pred_split <- rsample::initial_split(model_data_clean, strata = play_call)
 play_pred_training <- rsample::training(play_pred_split)
 play_pred_testing <- rsample::testing(play_pred_split)
-play_pred_folds <- rsample::vfold_cv(play_pred_training)
 
-play_call_recipe <- 
-  recipe(formula = play_call ~ ., data = play_pred_training) %>%
+set.seed(1985)
+play_pred_folds <- rsample::vfold_cv(play_pred_training, strata = play_call)
+
+#########
+## building recipe
+#########
+
+play_call_recipe <-
+  recipe(play_call ~ ., data = play_pred_training) %>%
   update_role(posteam, new_role = "posteam_id") %>%
   update_role(season, new_role = "season_id") %>%
-  step_dummy(all_nominal_predictors()) %>%
-  step_zv(all_predictors()) %>%
-  step_corr(all_predictors(), threshold = .7)
+  step_dummy(all_factor_predictors(), one_hot = TRUE)
 
-play_call_model <- multinom_reg(
-  penalty = .001,
-  mixture = 1) %>%
+play_call_model <-
+  multinom_reg(penalty = tune(), mixture = 1) %>%
+  set_mode("classification") %>%
   set_engine("glmnet", family = "multinomial")
 
 play_call_workflow <- workflow() %>%
   add_recipe(play_call_recipe) %>%
   add_model(play_call_model)
 
-sum(is.na(play_pred_training))
+play_call_workflow
+
+play_call_prepped <- prep(play_call_recipe)
+juice(play_call_prepped)
+
+play_call_grid <- grid_regular(penalty(range = c(-5, 0)), levels = 20)
+
+doParallel::registerDoParallel()
+set.seed(1988)
 
 tic()
-set.seed(123)
-play_call_fit <- fit(play_call_workflow, data = play_pred_training)
-toc()
+play_call_resample <-
+  tune_grid(
+    play_call_workflow,
+    play_pred_folds,
+    grid = play_call_grid)
+toc() ### 285 seconds
 
-play_call_predictions <- augment(play_call_fit, new_data = play_pred_testing)
+autoplot(play_call_resample)
 
-play_call_predictions %>%
-  select(starts_with(".pred")) %>%
-  head()
+show_best(play_call_resample)
 
-play_call_metrics <- metric_set(accuracy, roc_auc)
+final_penalty <-
+  play_call_resample %>%
+  select_by_one_std_err(metric = "roc_auc", desc(penalty))
 
-play_call_predictions %>%
-  play_call_metrics(play_call, .pred_long_pass:.pred_short_pass, estimate = .pred_class)
+final_play_call <- play_call_workflow %>%
+  finalize_workflow(final_penalty) %>%
+  last_fit(play_pred_split)
+
+collect_metrics(final_play_call)
+
+collect_predictions(final_play_call) %>%
+  roc_curve(truth = play_call, .pred_long_pass:.pred_short_pass) %>%
+  ggplot(aes(1 - specificity, sensitivity, color = .level)) +
+  geom_abline(slope = 1, lty = 2) +
+  geom_path(linewidth = 1.5)
+
+final_fitted <- extract_workflow(final_play_call)
+
+predict(final_fitted, play_pred_testing[2124, ], type = "prob")
 
 
-### combing training and testing into one dataframe
 all_data <- bind_rows(play_pred_training, play_pred_testing)
 
 ### running the predictions wholly
-all_predictions <- augment(play_call_fit, new_data = all_data)
-
-options(digits = 3)
+all_predictions <- augment(final_fitted, new_data = all_data)
 
 ### determing most/least predictable teams
 predictions_by_team <- all_predictions %>%
@@ -146,7 +180,33 @@ predictions_by_team <- all_predictions %>%
   group_by(season, posteam) %>%
   summarize(total_plays = n(),
             total_pred = sum(.pred_class == play_call),
-            pred_pct = total_pred / total_plays * 100)
+            pred_pct = total_pred / total_plays * 100) %>%
+  ungroup() %>%
+  arrange(-pred_pct) %>%
+  slice(1:10)
+
+teams <- nflreadr::load_teams(current = TRUE)
+
+predictions_by_team <- predictions_by_team %>%
+  left_join(teams, by = c("posteam" = "team_abbr"))
+
+options(digits = 3)
+
+predictions_by_team %>%
+  dplyr::select(season, team_logo_wikipedia, total_plays, total_pred, pred_pct) %>%
+  gt() %>%
+  tab_spanner(
+    label = "Most Predictable Offenses: 2006 - 2022",
+    columns = c("season", "team_logo_wikipedia", "total_plays", "total_pred", "pred_pct")) %>%
+  cols_label(
+    season = "Season",
+    team_logo_wikipedia = "",
+    total_plays = "# of Plays",
+    total_pred = "# Predicted",
+    pred_pct = "Percent Predicted") %>%
+  gt_img_rows(columns = team_logo_wikipedia, height = 25) %>%
+  gt_color_rows(pred_pct, palette = "ggsci::blue_material") %>%
+  gtExtras::gt_theme_espn()
 
 
 ##########################
@@ -162,6 +222,7 @@ max_count <- max(class_counts)
 
 # Calculate the baseline accuracy
 baseline_acc <- max_count / sum(class_counts)
+baseline_acc
 
 #### baseline accuracy is 30% (if the model always picked run_outside). Model is an 11% increase.
 
